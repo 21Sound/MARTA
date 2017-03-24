a3c74f8b2e790b716b4af279aed6adef75d000a5
ha1044 <hagen.jaeger@jade-hs.de>
2017-03-24 18:51:25 +0100

    Added comments and license.

--- CppRTA.h
+++ CppRTA.h
@@ -1,3 +1,9 @@
+/*----------------------------------------------------------------------------*\
+Header of CppRTA.cpp
+
+Author: (c) Hagen Jaeger    January 2017 - Now
+\*----------------------------------------------------------------------------*/
+
 #ifndef CPPRTA_H
 #define CPPRTA_H
 
@@ -141,3 +147,26 @@ private:
 #endif
 
 #endif
+
+//--------------------- License ------------------------------------------------
+
+// Copyright (c) 2017 Hagen Jaeger
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- QtRTAtst.cpp
+++ QtRTAtst.cpp
@@ -1,6 +1,13 @@
 #include <QApplication>
 #include "mainWindow.h"
 
+/*----------------------------------------------------------------------------*\
+Test routine for the C++ real time audio class with basic Qt library support.
+See mainWindow.cpp/.h for more informations.
+
+Author: (c) Uwe Simmer, Hagen Jaeger    January 2017 - Now
+\*----------------------------------------------------------------------------*/
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -13,4 +20,25 @@ int main(int argc, char *argv[])
     return app.exec();
 }
 
-//------------------------------------------------------------------------------
+//--------------------- License ------------------------------------------------
+
+// Copyright (c) 2017 Uwe Simmer, Hagen Jaeger
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
